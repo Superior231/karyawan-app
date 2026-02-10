383038ea32dce280ba63316e27af248ca46ec8f3
Hikmal Falah <ikmalfalah@gmail.com>
2026-02-11 01:36:35 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'username' => 'Admin',
+            'roles' => 'admin',
+            'password' => Hash::make('admin1234'),
+        ]);
+    }
+}
