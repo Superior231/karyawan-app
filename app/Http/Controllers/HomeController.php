b422638f255403361937e0052a92771c8f6225c6
Hikmal Falah <ikmalfalah@gmail.com>
2026-02-12 02:11:12 +0700

    Add {ui/ux}: information card -> dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
+use App\Models\Position;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,9 +25,18 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $employees = Employee::all();
+        $positions = Position::all();
+        $activeEnployees = Employee::where('status', 'active')->get();
+        $inactiveEnployees = Employee::where('status', 'inactive')->get();
+
         return view('pages.home.index', [
             'title' => 'Home - Karyawan App | PT Maju Jaya',
-            'active' => 'home'
+            'active' => 'home',
+            'employees' => $employees,
+            'positions' => $positions,
+            'activeEnployees' => $activeEnployees,
+            'inactiveEnployees' => $inactiveEnployees
         ]);
     }
 }
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,5 +1,72 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1 class="text-center">Selamat Datang Di Aplikasi Karyawan</h1>
+    <section class="dashboard">
+        <h1 class="text-center">Selamat Datang Di Aplikasi Karyawan</h1>
+
+        <div class="mt-4 row g-3">
+            <a href="{{ route('employee.index') }}" class="col">
+                <div class="card h-100">
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <div class="gap-2 d-flex">
+                            <div class="p-0 m-0 bg-icon-employee">
+                                <i class='p-3 fas fa-users fs-3'></i>
+                            </div>
+                            <div class="gap-1 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="py-0 my-0 text-nowrap text-secondary">Total Karyawan</h5>
+                                <h4 class="py-0 my-0 fw-bold">{{ $employees->count() }}</h4>
+                            </div>
+                        </div>
+                        <i class='p-3 bx bx-chevron-right fs-2'></i>
+                    </div>
+                </div>
+            </a>
+            <a href="{{ route('position.index') }}" class="col">
+                <div class="card h-100">
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <div class="gap-2 d-flex">
+                            <div class="p-0 m-0 bg-icon-position">
+                                <i class='p-3 bx bxs-label fs-2'></i>
+                            </div>
+                            <div class="gap-1 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="py-0 my-0 text-nowrap text-secondary">Total Jabatan</h5>
+                                <h4 class="py-0 my-0 fw-bold">{{ $positions->count() }}</h4>
+                            </div>
+                        </div>
+                        <i class='p-3 bx bx-chevron-right fs-2'></i>
+                    </div>
+                </div>
+            </a>
+            <div class="col">
+                <div class="card card-active h-100">
+                    <div class="card-body">
+                        <div class="gap-2 d-flex">
+                            <div class="p-0 m-0 bg-icon-active text-success">
+                                <i class='p-3 bx bxs-check-circle fs-2'></i>
+                            </div>
+                            <div class="gap-1 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="py-0 my-0 text-nowrap text-secondary">Total Karyawan Aktif</h5>
+                                <h4 class="py-0 my-0 fw-bold">{{ $activeEnployees->count() }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card card-inactive h-100">
+                    <div class="card-body">
+                        <div class="gap-2 d-flex">
+                            <div class="p-0 m-0 bg-icon-inactive text-danger">
+                                <i class='p-3 bx bxs-x-circle fs-2'></i>
+                            </div>
+                            <div class="gap-1 d-flex flex-column justify-content-center align-items-start">
+                                <h5 class="py-0 my-0 text-nowrap text-secondary">Total Karyawan Tidak Aktif</h5>
+                                <h4 class="py-0 my-0 fw-bold">{{ $inactiveEnployees->count() }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
